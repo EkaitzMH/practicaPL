dbdeddd983cf577448ebc9b1ba55f85cc9b234db
EkaitzMH <123484558+EkaitzMH@users.noreply.github.com>
2025-04-30 14:15:25 +0200

    Poner palabras y abandonar partida

--- scrabble.pl
+++ scrabble.pl
@@ -87,12 +87,149 @@ colocar_letra(F, C, L) :-
     casilla(F, C, libre, none),
     retract(casilla(F, C, libre, none)),
     assert(casilla(F, C, ocupada, L)).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% FORMAR PALABRA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% formar_palabra(+Jugador, +Orientacion, +Fila, +Columna, +Palabra)
+formar_palabra(J, O, F, C, P) :-
+    partida_activa(_),
+    jugador(J, _, Fichas),
+    atom_chars(P, Letras),
+    puede_colocar(P, O, F, C),
+    contiene_fichas(Fichas, Letras),
+    colocar_palabra(Letras, O, F, C),
+    actualizar_fichas_jugador(J, Letras),
+    sumar_puntos(J, Letras, O, F, C),
+    reponer_fichas(J).
+
+% puede_colocar(+Palabra, +Orientacion, +Fila, +Columna)
+puede_colocar(P, horizontal, F, C) :-
+    atom_length(P, L),
+    C2 is C + L - 1,
+    C2 =< 15,
+    L1 is L -1,
+    forall(between(0, L1, I), (
+        CPos is C + I,
+        casilla(F, CPos, libre, _) ; casilla(F, CPos, ocupada, _)
+    )).
+puede_colocar(P, vertical, F, C) :-
+    atom_length(P, L),
+    F2 is F + L - 1,
+    F2 =< 15,
+    L1 is L -1,
+    forall(between(0, L1, I), (
+        FPos is F + I,
+        casilla(FPos, C, libre, _) ; casilla(FPos, C, ocupada, _)
+    )).
+
+% contiene_fichas(+FichasJugador, +LetrasPalabra)
+contiene_fichas(FJ, Ls) :-
+    msort(FJ, S1), msort(Ls, S2), sublista(S2, S1).
+
+% sublista(S, L): S está contenida en L
+sublista([], _).
+sublista([X|Xs], [X|Ys]) :- sublista(Xs, Ys).
+sublista(Xs, [_|Ys]) :- sublista(Xs, Ys).
+
+% colocar_palabra(+Letras, +O, +F, +C)
+colocar_palabra([], _, _, _).
+colocar_palabra([L|Ls], horizontal, F, C) :-
+    colocar_letra(F, C, L),
+    C1 is C + 1,
+    colocar_palabra(Ls, horizontal, F, C1).
+colocar_palabra([L|Ls], vertical, F, C) :-
+    colocar_letra(F, C, L),
+    F1 is F + 1,
+    colocar_palabra(Ls, vertical, F1, C).
+
+% actualizar_fichas_jugador(+J, +LetrasUsadas)
+actualizar_fichas_jugador(J, Letras) :-
+    jugador(J, P, F),
+    remove_letras(F, Letras, FR),
+    retract(jugador(J, P, _)),
+    assert(jugador(J, P, FR)).
+
+% remove_letras(+FichasJugador, +LetrasUsadas, -Restantes)
+remove_letras(F, [], F).
+remove_letras(F, [L|Ls], R) :-
+    select(L, F, F1),
+    remove_letras(F1, Ls, R).
+
+% sumar_puntos(+J, +Letras, +O, +F, +C)
+sumar_puntos(J, Letras, _, _, _) :-
+    length(Letras, N),
+    Puntos is N * 1, % Simulación: 1 punto por letra
+    jugador(J, Prev, Fichas),
+    retract(jugador(J, Prev, Fichas)),
+    Nuevo is Prev + Puntos,
+    assert(jugador(J, Nuevo, Fichas)),
+    format("~w ha sumado ~d puntos.~n", [J, Puntos]).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ABANDONAR PARTIDA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% abandonar_partida(+Jugador)
+% abandonar_partida(+Jugador)
+abandonar_partida(J) :-
+    (   partida_activa(jugadores(J, Otro)) ; partida_activa(jugadores(Otro, J))
+    ->  format("El jugador ~w ha abandonado la partida. ~w gana.~n", [J, Otro]),
+        retractall(partida_activa(_)),
+        retractall(jugador(_, _, _)),
+        retractall(casilla(_, _, _, _)),
+        retractall(ficha_disponible(_, _))
+    ;   partida_activa(_)
+    ->  format("Error: el jugador ~w no participa en la partida actual.~n", [J]), fail
+    ;   format("Error: no hay ninguna partida activa.~n"), fail
+    ).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% FUNCIONES AUXILIARES
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% reponer_fichas(+Jugador)
+reponer_fichas(J) :-
+    jugador(J, P, FichasAct),
+    length(FichasAct, N),
+    M is 7 - N,
+    total_fichas_disponibles(Bolsa),
+    min(M, Bolsa, CantidadAReponer),
+    generar_fichas(CantidadAReponer, Nuevas),
+    append(FichasAct, Nuevas, Final),
+    retract(jugador(J, P, _)),
+    assert(jugador(J, P, Final)).
+
+% total_fichas_disponibles(-N)
+total_fichas_disponibles(N) :-
+    findall(L, ficha_disponible(L, _), Ls),
+    length(Ls, N).
+
+% min/3 para obtener el mínimo entre dos valores
+min(A, B, A) :- A =< B, !.
+min(_, B, B).
+
+% actualizar generar_fichas para usar la bolsa
 % generar_fichas(+N, -Fichas)
+generar_fichas(0, []).
+generar_fichas(N, [L|R]) :-
+    N > 0,
+    findall(F, ficha_disponible(F, _), Bolsa),
+    Bolsa \= [],
+    random_member(L, Bolsa),
+    retract(ficha_disponible(L, _)),
+    N1 is N - 1,
+    generar_fichas(N1, R).
+
+reset_juego :- 
+    retractall(partida_activa(_)),
+    retractall(jugador(_, _, _)),
+    retractall(casilla(_, _, _, _)),
+    retractall(ficha_disponible(_, _)).
+
+
+% letras_disponibles/1 inicializa la bolsa solo si está vacía
 generar_fichas(0, []).
 generar_fichas(N, [L|R]) :-
     N > 0,
@@ -104,6 +241,7 @@ generar_fichas(N, [L|R]) :-
 % letras_disponibles(-Lista)
 letras_disponibles(['A','B','C','D','E','F','G','H','I','J','K','L','M',
                     'N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z','_']).
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% A COMPLETAR POSTERIORMENTE:
